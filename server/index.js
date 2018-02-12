de067b3129d54ecf49c0412f069a73c667e5c262
tcscheurer <trevor.scheurer1@gmail.com>
2018-02-12 14:36:47 -0600

    Completed node 3 mini

--- server/index.js
+++ server/index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const mc = require( `./controllers/messages_controller` );
+require('dotenv').config();
+
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 10000}
+}));
+
+app.use((req,res,next)=> createInitialSession(req,res,next));
+app.use((req,res,next)=> {
+    const {method} = req;
+    if(method === 'POST' || method === 'PUT'){
+        filter(req,res,next);
+    }else{
+        next();
+    }
+});
 
 const messagesBaseUrl = "/api/messages";
+app.get('/api/messages/history', mc.history);
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
 app.put( `${messagesBaseUrl}`, mc.update );
